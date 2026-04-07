a9925da4d3a3d5198247bffae7980e797b966edf
Team SillyGuy Balatro <102820358+KKingOfficial@users.noreply.github.com>
2026-04-06 23:25:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 (function() {
     var showToast = vendetta.ui.toasts.showToast;
-    var findByName = vendetta.metro.findByName;
     var findByProps = vendetta.metro.findByProps;
 
     return {
         onLoad: function() {
-            var m1 = findByName("MessagesConnected", false);
-            var m2 = findByName("MessagesConnected", true);
-            var m3 = findByProps("HACK_fixModalInteraction");
+            var candidates = [
+                "stickyVisiblity",
+                "jumpToChatText",
+                "loadCachedMessages",
+                "renderLatestMessages",
+                "displayLatestMessages",
+                "firstRenderAfterReadyPayload"
+            ];
 
-            var themeKeys = "none";
-            try {
-                var t = vendetta.themes;
-                themeKeys = t ? Object.keys(t).slice(0,2).join(",") : "null";
-            } catch(e) { themeKeys = "err:" + e.message; }
+            var results = [];
+            candidates.forEach(function(prop) {
+                var m = findByProps(prop);
+                if (m) results.push(prop + "=" + typeof m);
+            });
 
-            var bunnyThemeKeys = "none";
-            try {
-                var bt = bunny.themes;
-                bunnyThemeKeys = bt ? Object.keys(bt).slice(0,2).join(",") : "null";
-            } catch(e) { bunnyThemeKeys = "err:" + e.message; }
-
-            showToast(
-                "m1=" + (m1?"y":"n") +
-                " m2=" + (m2?"y":"n") +
-                " m3=" + (m3?"y":"n") +
-                " vt=" + themeKeys +
-                " bt=" + bunnyThemeKeys
-            );
+            showToast(results.length ? results.join(" ") : "all null");
         },
         onUnload: function() {}
     };
