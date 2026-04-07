cfe40fba875b2afe13a1dba47aa2a417b10a6d18
Team SillyGuy Balatro <102820358+KKingOfficial@users.noreply.github.com>
2026-04-06 23:36:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 (function() {
-    var showToast = vendetta.ui.toasts.showToast;
     var findByProps = vendetta.metro.findByProps;
 
     return {
         onLoad: function() {
-            var r1 = [];
-            ["stickyVisiblity","jumpToChatText","loadCachedMessages"].forEach(function(p) {
-                r1.push(p.slice(0,8) + "=" + (findByProps(p) ? "y" : "n"));
-            });
-            showToast(r1.join(" "));
+            var candidates = [
+                "stickyVisiblity",
+                "jumpToChatText",
+                "loadCachedMessages",
+                "renderLatestMessages",
+                "displayLatestMessages",
+                "firstRenderAfterReadyPayload"
+            ];
 
-            setTimeout(function() {
-                var r2 = [];
-                ["renderLatestMessages","displayLatestMessages","firstRenderAfterReadyPayload"].forEach(function(p) {
-                    r2.push(p.slice(0,8) + "=" + (findByProps(p) ? "y" : "n"));
-                });
-                showToast(r2.join(" "));
-            }, 2000);
+            var results = candidates.map(function(p) {
+                return p + "=" + (findByProps(p) ? "y" : "n");
+            }).join("\n");
+
+            alert(results);
         },
         onUnload: function() {}
     };
