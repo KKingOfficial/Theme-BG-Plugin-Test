cb94e57ed4cbb4144a08b668dcebad95e7ae1ccc
Team SillyGuy Balatro <102820358+KKingOfficial@users.noreply.github.com>
2026-04-06 23:57:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
-(function() { 
+(function() {
+    var after = vendetta.patcher.after;
     var findByProps = vendetta.metro.findByProps;
+    var React = vendetta.metro.common.React;
+    var RN = vendetta.metro.common.ReactNative;
+
+    function getBg() {
+        try {
+            var selected = Object.values(vendetta.themes).find(function(t) { return t.selected; });
+            if (!selected) return null;
+            var bg = selected.data && selected.data.background;
+            if (!bg || !bg.url) return null;
+            return { url: bg.url, opacity: bg.alpha != null ? bg.alpha : 0.5 };
+        } catch(e) { return null; }
+    }
+
+    var unpatches = [];
 
     return {
         onLoad: function() {
-            var props = ["loadCachedMessages", "renderLatestMessages", "displayLatestMessages", "firstRenderAfterReadyPayload"];
-            var out = [];
-            props.forEach(function(p) {
-                var m = findByProps(p);
-                if (m) {
-                    var keys = Object.keys(m).slice(0, 8);
-                    out.push(p.slice(0,10) + ":\n  " + keys.join(", "));
-                }
-            });
-            alert(out.join("\n\n"));
+            var chatModule = findByProps("ChatViewWrapper", "useChatLayout");
+            var target = chatModule && (chatModule.ChatViewWrapper || chatModule.default);
+
+            if (!target) {
+                chatModule = findByProps("ChatViewWrapperBase");
+                target = chatModule && (chatModule.ChatViewWrapperBase || chatModule.default);
+            }
+
+            if (!target) return;
+
+            var renderFn = target.render || target;
+
+            unpatches.push(after("render", renderFn, function(args, ret) {
+                var bg = getBg();
+                if (!bg || !ret) return;
+
+                return React.createElement(
+                    RN.View,
+                    { style: { flex: 1 } },
+                    React.createElement(RN.Image, {
+                        source: { uri: bg.url },
+                        style: {
+                            position: "absolute",
+                            top: 0, left: 0,
+                            right: 0, bottom: 0,
+                            width: "100%",
+                            height: "100%",
+                            opacity: bg.opacity
+                        },
+                        resizeMode: "cover"
+                    }),
+                    ret
+                );
+            }));
         },
-        onUnload: function() {}
+        onUnload: function() {
+            unpatches.forEach(function(u) { u(); });
+        }
     };
 })();
